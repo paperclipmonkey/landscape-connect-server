8fa6317c5774a6dd85252267567bb1cf89b70f12
Michael Waterworth <mike@3equals.co.uk>
2016-02-09 12:10:27 +0000

    Add public questionnaire-viewing site

---
+++ js/custom/publicQuestionnairesController.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.questions')
+        .controller('publicQuestionnairesCtrl', publicQuestionnairesCtrl);
+    /*jshint -W055 */
+    publicQuestionnairesCtrl.$inject = ['$filter', '$resource', 'questionnairesDataService', '$scope', '$http', '$state'];
+    function publicQuestionnairesCtrl($filter, $resource, questionnairesDataService, $scope, $http, $state) {
+    	$scope.questionnaires = [];
+
+	    $http.get("/api/questionnaires/").success(function(data, status) {
+	        $scope.questionnaires = data.result;
+	    })
+	}
+})();
---
+++ js/custom/publicQuestionniareController.js
@@ -0,0 +1,20 @@
+/**=========================================================
+ * Module: questionnaires.controller.js
+ * Controller for questionnairess
+ =========================================================*/
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app.questions')
+        .controller('publiQuestionnaireCtrl', publicQuestionnaireCtrl);
+    /*jshint -W055 */
+    publicQuestionnaireCtrl.$inject = ['$scope', '$http', '$stateParams', '$state'];
+    function publicQuestionnaireCtrl($scope, $http, $stateParams, $state) {
+        $http.get("/api/questionnaires/" + $stateParams.questionnaireId).success(function(data, status) {
+            console.log(data, status)
+            $scope.questionnaire = data;
+        })
+    }
+})();
