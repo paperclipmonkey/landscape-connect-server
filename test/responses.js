7255f2ea500575230cca769c769bae3e678e5210
Michael Waterworth <mike@3equals.co.uk>
2016-03-02 18:50:05 +0000

    Update tests using more files

--- test/responses.js
+++ test/responses.js
@@ -32,7 +32,48 @@ describe('Responses', function () {
       .field('data[key]', 'value')
       .field('data[foo]', 'bar')
       .field('data[biz]', 'baz')
-      .attach('photo', __dirname + '/data/example.jpg')
+      .attach('media1', __dirname + '/data/example1.jpg')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+  })
+
+  it('POST /api/questionnaires/x/responses with multiple photos should save new response', function (done) {
+    this.timeout(5000);
+    rAgent
+      .post('/api/questionnaires/' + testQId + '/responses')
+      .field('questionnaire', testQId)
+      .field('timestamp', new Date().getTime()) // Millis since epoch
+      .field('lat', (Math.random() * 360) - 180)
+      .field('lng', (Math.random() * 180) - 90)
+      .field('locAcc', Math.floor(Math.random() * 100)) // 0 - 100 Integer test
+      .field('data[key]', 'value')
+      .field('data[foo]', 'bar')
+      .field('data[biz]', 'baz')
+      .attach('media1', __dirname + '/data/example1.jpg')
+      .attach('media2', __dirname + '/data/example2.jpg')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err)
+        done()
+      })
+  })
+
+  it('POST /api/questionnaires/x/responses with MP3 should save new response', function (done) {
+    this.timeout(5000);
+    rAgent
+      .post('/api/questionnaires/' + testQId + '/responses')
+      .field('questionnaire', testQId)
+      .field('timestamp', new Date().getTime()) // Millis since epoch
+      .field('lat', (Math.random() * 360) - 180)
+      .field('lng', (Math.random() * 180) - 90)
+      .field('locAcc', Math.floor(Math.random() * 100)) // 0 - 100 Integer test
+      .field('data[key]', 'value')
+      .field('data[foo]', 'bar')
+      .field('data[biz]', 'baz')
+      .attach('media1', __dirname + '/data/example1.mp3')
       .expect(200)
       .end(function (err, res) {
         if (err) return done(err)
