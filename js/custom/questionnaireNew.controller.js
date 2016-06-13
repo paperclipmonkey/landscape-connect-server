6ee8d8aebc5bf85276dc87d0f81aa76ad7e0e74d
Michael Waterworth <mike@3equals.co.uk>
2016-06-13 18:28:10 +0100

    Check for duplicate questions

--- js/custom/questionnaireNew.controller.js
+++ js/custom/questionnaireNew.controller.js
@@ -106,6 +106,21 @@
         }
       ]
 
+      $scope.checkDuplicate = function(a,b){
+        console.log(a, b)
+        //Loop through selected section
+        var count = 0
+        for (var i = $scope.selectedSection.questions.length - 1; i >= 0; i--) {
+          var item = $scope.selectedSection.questions[i]
+          if(item.title === a){
+            count++
+          }
+        }
+        if(count > 1){//We have a duplicate
+          alert("Please use a unique question name")
+        }
+      }
+
       $scope.draggables = questionPrototypes.map(function (x) {
         return [x]
       })
